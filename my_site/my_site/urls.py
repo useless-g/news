eea71a5a224d3174f024a90ba1bda82896095590
Serg Bychenkov <uzellessmovic@gmail.com>
2022-01-26 14:50:05 +0300

    created some notes to DB

--- my_site/my_site/urls.py
+++ my_site/my_site/urls.py
@@ -18,11 +18,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from news.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('news.urls')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += [static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
