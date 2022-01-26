6de5d24075d7806ccef0c057ef334894a63078d8
Serg Bychenkov <uzellessmovic@gmail.com>
2022-01-26 23:28:27 +0300

    created template for main page deleted test page

--- my_site/news/urls.py
+++ my_site/news/urls.py
@@ -3,5 +3,4 @@ from .views import *
 
 urlpatterns = [
     path('', index),
-    path('my_page/', my_page),
 ]
--- my_site/news/views.py
+++ my_site/news/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import News
 
 
 def index(request):
-    print(request)
-    return HttpResponse('Hello, world!')
-
-
-def my_page(request):
-    return HttpResponse("""<!doctype html>
-<html>
-    <head>
-        <title>моя страница</title>
-        <meta charset="UTF-8" />
-    </head>
-    <body>
-        <h1>Hello world!</h1>
-        <h2>Hello world!</h2>
-        <h3>Hello world!</h3>
-        <h4>Hello world!</h4>
-        <h5>Hello world!</h5>
-        <h6>Hello world!</h6>
-        <p>bla bla bla</p>
-    </body>
-</html>""")
+    news = News.objects.order_by('-created_at')
+    context = {
+        'title': 'Новости',
+        'news': news
+    }
+    return render(request, 'news/index.html', context)
