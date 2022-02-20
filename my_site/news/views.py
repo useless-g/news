60207138c410c9ab6870e35f81e7aff03db947e7
Serg Bychenkov <uzellessmovic@gmail.com>
2022-02-20 16:50:29 +0300

    added category refs changed urls

--- my_site/news/views.py
+++ my_site/news/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import *
 
 
 def index(request):
     news = News.objects.order_by('-created_at')
+    categories = Category.objects.all()
     context = {
         'title': 'Новости',
-        'news': news
+        'news': news,
+        'categories': categories
     }
     return render(request, 'news/index.html', context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id).order_by('-created_at')
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
+        'category': category,
+        'categories': categories,
+    }
+    return render(request, 'news/get_category.html', context)
