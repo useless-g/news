aa203333ea099b407bae23ce70f8db6ea932ab58
Serg Bychenkov <uzellessmovic@gmail.com>
2022-02-20 15:49:55 +0300

    added categories

--- my_site/news/admin.py
+++ my_site/news/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
-from .models import News
+from .models import *
 
 
 class NewsAdmin(admin.ModelAdmin):
     # поля которые мы хотим видеть
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+    list_display = ('id', 'title', 'category', 'created_at', 'updated_at', 'is_published')
     # поля которые должны быть ссылками на запись в модели
     list_display_links = ('id', 'title')
     # поля по которым адмни может проводить поиск
     search_fields = ('title', 'content')
+    list_editable = ('is_published', )
+    list_filter = ('is_published', 'category')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # поля которые мы хотим видеть
+    list_display = ('id', 'title')
+    # поля которые должны быть ссылками на запись в модели
+    list_display_links = ('id', 'title')
+    # поля по которым адмни может проводить поиск
+    search_fields = ('title', )
 
 
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- my_site/news/models.py
+++ my_site/news/models.py
@@ -8,6 +8,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='дата обновления')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='опубликовано')
+    category = models.ForeignKey('Category', models.PROTECT, null=True, blank=True, verbose_name='категория')
 
     def __str__(self):
         return self.title
@@ -16,3 +17,15 @@ class News(models.Model):
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['-created_at', 'title']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, verbose_name='наименование',db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title', ]
